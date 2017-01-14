f16fe248237064ef2c99e1c2e7c8131e03e679cb
595645330 <7758258867ysy!?>
2017-01-14 02:36:00 -0800

    wsws

--- src/Math.ts
+++ src/Math.ts
@@ -3,6 +3,8 @@
  */
 
 import Log from "./Util";
+import * as rp from "request-promise-native";
+import {error} from "util";
 
 interface IMath {
     add(urls: string[]): Promise<number>;
@@ -17,14 +19,124 @@ export default class Math implements IMath {
     add(urls: string[]): Promise<number> {
         return new Promise(function (fulfill, reject) {
             // TODO: implement
-            fulfill(0);
+            //---------------------------------------------------------------------------------
+            let nums : number = 0;
+            let ifnum: boolean = false;
+            let processList:Promise<any>[] = [];
+            if (urls === undefined || urls.length == 0) {
+                fulfill(0);
+            }
+            for(let k = 0; k < urls.length; k++) {
+                var options = {uri: urls[k],};
+                processList.push(rp(options));
+            }
+            Promise.all(processList).then(function(num:string[]){
+                //console.log(num);
+
+                for(let re of num){
+                    let parsedJson: {[id: string]: any;};
+                    try {
+                        parsedJson = JSON.parse(re);
+                    }catch(e){
+                        reject('Error: URL could not be retrieved');
+                    }
+                    // 1. For Json Array
+                    if(parsedJson instanceof  Array){
+                        for(let z of parsedJson){
+                            if(typeof z === 'number'){
+                                nums = nums + z;
+                                ifnum = true;
+                            }
+                        }
+                    }
+                    // 2. For Json Object
+                    else{
+                        let jsonString: string[] = Object.keys(parsedJson);
+                        //console.log(jsonString);
+                        for (let x of jsonString) {
+                            if (parsedJson[x] instanceof Array) {
+                                for (let y of parsedJson[x]) {
+                                    if (typeof y === 'number') {
+                                        nums = nums + y;
+                                        ifnum =true;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                if(ifnum){
+                    fulfill(nums);
+                }
+                else{
+                    reject('Error: No number was provided');
+                }
+            }).catch(function(err) {
+                //console.log('sasasas');
+                Log.error("Creation failed: "+ err);
+            });
         });
     }
 
     multiply(urls: string[]): Promise<number> {
         return new Promise(function (fulfill, reject) {
             // TODO: implement
-            fulfill(0)
+            let nums : number = 1;
+            let ifnum: boolean = false;
+            let processList:Promise<any>[] = [];
+            if (urls === undefined || urls.length == 0) {
+                fulfill(0);
+            }
+            for(let k = 0; k < urls.length; k++) {
+                var options = {uri: urls[k],};
+                processList.push(rp(options));
+            }
+            Promise.all(processList).then(function(num:string[]){
+                //console.log(num);
+
+                for(let re of num){
+                    let parsedJson: {[id: string]: any;};
+                    try {
+                        parsedJson = JSON.parse(re);
+                    }catch(e){
+                        reject('Error: Could not parse JSON');
+                    }
+                    // 1. For Json Array
+                    if(parsedJson instanceof  Array){
+                        for(let z of parsedJson){
+                            if(typeof z === 'number'){
+                                nums = nums * z;
+                                ifnum = true;
+                            }
+                        }
+                    }
+                    // 2. For Json Object
+                    else{
+                        let jsonString: string[] = Object.keys(parsedJson);
+                        //console.log(jsonString);
+
+                        for (let x of jsonString) {
+                            if (parsedJson[x] instanceof Array) {
+                                for (let y of parsedJson[x]) {
+                                    if (typeof y === 'number') {
+                                        nums = nums * y;
+                                        ifnum =true;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                if(ifnum){
+                    fulfill(nums);
+                }
+                else{
+                    reject('Error: No number was provided');
+                }
+            }).catch(function(err) {
+                //console.log('sasasas');
+                Log.error("Creation failed: "+ err);
+            });
         });
     }
 }
--- test/MathSpec.ts
+++ test/MathSpec.ts
@@ -18,7 +18,216 @@ describe("MathSpec", function () {
         math = null;
     });
 
-    it("Add should return 0 if no valid arrays are found", function () {
+    // it("Add should return 0 if no valid arrays are found", function () {
+    //     return math.add([]).then(function (value: number) {
+    //         Log.test('Value: ' + value);
+    //         expect(value).to.equal(0);
+    //     }).catch(function (err) {
+    //         Log.test('Error: ' + err);
+    //         expect.fail();
+    //     })
+    // });
+
+    it("Add should return 6", function () {
+        return math.add(['http://skaha.cs.ubc.ca:11313/7b77.json']).then(function (value: number) {
+            Log.test('Value: ' + value);
+            expect(value).to.equal(6);
+        }).catch(function (err) {
+            Log.test('Error: ' + err);
+            expect.fail();
+        })
+    });
+
+    it("Add should return 12", function () {
+        return math.add(['http://skaha.cs.ubc.ca:11313/4968.json']).then(function (value: number) {
+            Log.test('Value: ' + value);
+            expect(value).to.equal(12);
+        }).catch(function (err) {
+            Log.test('Error: ' + err);
+            expect.fail();
+        })
+    });
+
+    it("Add should return 18", function () {
+        return math.add(['http://skaha.cs.ubc.ca:11313/4968.json','http://skaha.cs.ubc.ca:11313/7b77.json']).then(function (value: number) {
+            Log.test('Value: ' + value);
+            expect(value).to.equal(18);
+        }).catch(function (err) {
+            Log.test('Error: ' + err);
+            expect.fail();
+        })
+    });
+
+    it("Add should return 12", function () {
+        return math.add(['http://skaha.cs.ubc.ca:11313/822d.json', 'http://skaha.cs.ubc.ca:11313/4968.json']).then(function (value: number) {
+            Log.test('Value: ' + value);
+            expect(value).to.equal(12);
+        }).catch(function (err) {
+            Log.test('Error: ' + err);
+            expect.fail();
+        })
+    });
+
+    it("Add should return 12", function () {
+        return math.add([ 'http://skaha.cs.ubc.ca:11313/4968.json','http://skaha.cs.ubc.ca:11313/822d.json']).then(function (value: number) {
+            Log.test('Value: ' + value);
+            expect(value).to.equal(12);
+        }).catch(function (err) {
+            Log.test('Error: ' + err);
+            expect.fail();
+        })
+    });
+
+    it("Add should return error if no numbers are found", function () {
+        return math.add(['http://skaha.cs.ubc.ca:11313/822d.json']).then(function (value: number) {
+            //console.log(value);
+            Log.test('Value: ' + value);
+            expect.fail();
+        }).catch(function (err) {
+            Log.test(err);
+            expect(err).to.equal('Error: No number was provided');
+        })
+    });
+
+    it("Add should return error if no valid arrays are found", function () {
+        return math.add(['http://skaha.cs.ubc.ca:11313/jdw3.json']).then(function (value: number) {
+            //console.log(value);
+            Log.test('Value: ' + value);
+            expect.fail();
+        }).catch(function (err) {
+            Log.test(err);
+            expect(err).to.equal('Error: URL could not be retrieved');
+        })
+    });
+
+    it("Add should return error if no valid arrays are found", function () {
+        return math.add(['http://skaha.cs.ubc.ca:11313/jdw3.json','http://skaha.cs.ubc.ca:11313/4968.json']).then(function (value: number) {
+            //console.log(value);
+            Log.test('Value: ' + value);
+            expect.fail();
+        }).catch(function (err) {
+            Log.test(err);
+            expect(err).to.equal('Error: URL could not be retrieved');
+        })
+    });
+
+    it("Add should return error if no valid arrays are found", function () {
+        return math.add(['http://skaha.cs.ubc.ca:11313/4968.json','http://skaha.cs.ubc.ca:11313/jdw3.json']).then(function (value: number) {
+            //console.log(value);
+            Log.test('Value: ' + value);
+            expect.fail();
+        }).catch(function (err) {
+            Log.test(err);
+            expect(err).to.equal('Error: URL could not be retrieved');
+        })
+    });
+
+    it("Multiply should return 6", function () {
+        return math.multiply(['http://skaha.cs.ubc.ca:11313/7b77.json']).then(function (value: number) {
+            Log.test('Value: ' + value);
+            expect(value).to.equal(6);
+        }).catch(function (err) {
+            Log.test('Error: ' + err);
+            expect.fail();
+        })
+    });
+
+    it("Multiply should return 40", function () {
+        return math.multiply(['http://skaha.cs.ubc.ca:11313/4968.json']).then(function (value: number) {
+            Log.test('Value: ' + value);
+            expect(value).to.equal(40);
+        }).catch(function (err) {
+            Log.test('Error: ' + err);
+            expect.fail();
+        })
+    });
+
+    it("Multiply should return 240", function () {
+        return math.multiply(['http://skaha.cs.ubc.ca:11313/4968.json','http://skaha.cs.ubc.ca:11313/7b77.json']).then(function (value: number) {
+            Log.test('Value: ' + value);
+            expect(value).to.equal(240);
+        }).catch(function (err) {
+            Log.test('Error: ' + err);
+            expect.fail();
+        })
+    });
+
+    it("Multiply should return 40", function () {
+        return math.multiply(['http://skaha.cs.ubc.ca:11313/822d.json', 'http://skaha.cs.ubc.ca:11313/4968.json']).then(function (value: number) {
+            Log.test('Value: ' + value);
+            expect(value).to.equal(40);
+        }).catch(function (err) {
+            Log.test('Error: ' + err);
+            expect.fail();
+        })
+    });
+
+    it("Multiply should return error if no numbers are found", function () {
+        return math.multiply(['http://skaha.cs.ubc.ca:11313/822d.json']).then(function (value: number) {
+            //console.log(value);
+            Log.test('Value: ' + value);
+            expect.fail();
+        }).catch(function (err) {
+            Log.test(err);
+            expect(err).to.equal('Error: No number was provided');
+        })
+    });
+
+    it("Multiply should return error if no valid arrays are found", function () {
+        return math.multiply(['http://skaha.cs.ubc.ca:11313/jdw3.json']).then(function (value: number) {
+            //console.log(value);
+            Log.test('Value: ' + value);
+            expect.fail();
+        }).catch(function (err) {
+            Log.test(err);
+            expect(err).to.equal('Error: Could not parse JSON');
+        })
+    });
+
+    it("Multiply should return error if no valid arrays are found", function () {
+        return math.multiply(['http://skaha.cs.ubc.ca:11313/jdw3.json','http://skaha.cs.ubc.ca:11313/4968.json']).then(function (value: number) {
+            //console.log(value);
+            Log.test('Value: ' + value);
+            expect.fail();
+        }).catch(function (err) {
+            Log.test(err);
+            expect(err).to.equal('Error: Could not parse JSON');
+        })
+    });
+
+    it("Multiply should return error if no valid arrays are found", function () {
+        return math.multiply(['http://skaha.cs.ubc.ca:11313/4968.json','http://skaha.cs.ubc.ca:11313/jdw3.json']).then(function (value: number) {
+            //console.log(value);
+            Log.test('Value: ' + value);
+            expect.fail();
+        }).catch(function (err) {
+            Log.test(err);
+            expect(err).to.equal('Error: Could not parse JSON');
+        })
+    });
+
+    it("Multiply should return error if empty arrays are found", function () {
+        return math.multiply(['http://skaha.cs.ubc.ca:11313/0.json']).then(function (value: number) {
+            //console.log(value);
+            Log.test('Value: ' + value);
+            expect.fail();
+        }).catch(function (err) {
+            Log.test(err);
+            expect(err).to.equal('Error: No number was provided');
+        })
+    });
+
+    it("Multiply should return 0", function () {
+        return math.multiply([]).then(function (value: number) {
+            Log.test('Value: ' + value);
+            expect(value).to.equal(0);
+        }).catch(function (err) {
+            Log.test('Error: ' + err);
+            expect.fail();
+        })
+    });
+
+    it("Add should return 0", function () {
         return math.add([]).then(function (value: number) {
             Log.test('Value: ' + value);
             expect(value).to.equal(0);
@@ -27,4 +236,5 @@ describe("MathSpec", function () {
             expect.fail();
         })
     });
+
 });
